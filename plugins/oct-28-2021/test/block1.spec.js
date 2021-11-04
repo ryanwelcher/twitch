0740bbe57041bc82df14b2c15e875d918549bc0e
Ryan Welcher <ryan.welcher@automattic.com>
2021-11-04 13:27:00 -0400

    Add jest version to get linting working and relint tests.

--- plugins/oct-28-2021/test/block1.spec.js
+++ plugins/oct-28-2021/test/block1.spec.js
@@ -5,21 +5,20 @@ import {
 	createNewPost,
 	insertBlock,
 	getEditedPostContent,
-	wpDataSelect,
-	getAllBlocks
-} from "@wordpress/e2e-test-utils";
+	getAllBlocks,
+} from '@wordpress/e2e-test-utils';
 
 // Increase the timeout limit for this test.
 jest.setTimeout(30000);
 
-describe("Block 1 Tests", () => {
+describe('Block 1 Tests', () => {
 	beforeAll(async () => {
 		// Create a post
 		await createNewPost();
-		await insertBlock("Oct 28 2021");
+		await insertBlock('Oct 28 2021');
 	});
 
-	it("block is inserted with empty message attribute", async () => {
+	it('block is inserted with empty message attribute', async () => {
 		// Was the block inserted?
 		expect(
 			await page.$('[data-type="create-block/oct-28-2021"]')
@@ -33,7 +32,7 @@ describe("Block 1 Tests", () => {
 	`);
 	});
 
-	it("updates the message attribute", async () => {
+	it('updates the message attribute', async () => {
 		// const blocks = await wpDataSelect("core/block-editor", "getBlocks");
 		// In the live stream, I used the code above to get the blocks, turns out that getAllBlocks does the same thing.
 		const blocks = await getAllBlocks();
@@ -42,7 +41,7 @@ describe("Block 1 Tests", () => {
 
 		await page.waitForSelector(`#block-${clientId}`);
 		await page.focus(`#block-${clientId}`);
-		await page.keyboard.type("This is a test!");
+		await page.keyboard.type('This is a test!');
 
 		// Was the block inserted?
 		expect(blocks).not.toBeNull();
