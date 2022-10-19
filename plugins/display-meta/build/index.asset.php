73ddec8b8bc58aec31424c446643372212793521
Ryan Welcher <me@ryanwelcher.com>
2022-10-19 12:25:49 -0400

    Remove build folders from versioning.

--- plugins/display-meta/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '503d82525127d463db4d');
